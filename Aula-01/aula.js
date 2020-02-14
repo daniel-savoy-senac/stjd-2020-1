e63631849877b257fcc66603e6853feb2ab1d74e
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2020-02-13 22:09:30 -0300

    cores

--- Aula-01/aula.js
+++ Aula-01/aula.js
@@ -2,8 +2,13 @@ window.addEventListener("mousemove", colorir);
 
 function colorir(event){
     console.log(`X: ${event.x} Y:${event.y}`);
-    var matiz = Math.random() * 360;
+    // var matiz = Math.random() * 360;
+
+    var w = window.innerWidth;
+    var x = event.x;
+    var hue = x / w * 360;
+
     // HUE SATURATION LIGHT
     // MATIZ SATURACAO LUMINOZIDADE
-    document.body.style.backgroundColor = `hsl(${matiz}deg,100%,50%)`; 
+    document.body.style.backgroundColor = `hsl(${hue}deg,100%,50%)`; 
 }
