fa424e9f8fa0b39adc84ecb8d667525c0adc397d
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2020-02-20 21:41:24 -0300

    esqueleto

---
+++ Aula-02/aula.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", main);
+
+let canvas,
+    gl,
+    vertexShaderSource,
+    fragmentShaderSource,
+    vertexShader,
+    fragmentShader,
+    shaderProgram,
+    data,
+    positionAttr,
+    positionBuffer,
+    frame;
+
+
+function getCanvas(){
+
+}
+
+async function main(){
+
+    // 1 - Carregar a área de desenho (canvas)
+
+    // 2 - Carregar a API do WebGL (contexto)
+
+    // 3 - Carregar os arquivos de shader (.glsl)
+
+    // 4 - Compilar os shaders
+
+    // 5 - Link dos shaders
+
+    // 6 - Criar dados (modelo)
+
+    // 7 - Transferir dados para GPU
+
+    // 8 - Chamar o loop de redesenho
+
+}
+
