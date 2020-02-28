4823f9928afdc2678dfe4adc3177f03ea918f5a2
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2020-02-27 21:22:15 -0300

    cores

--- Aula-02/fragment.glsl
+++ Aula-02/fragment.glsl
@@ -1,5 +1,8 @@
 precision highp float;
 
+varying vec2 color;
+
 void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    float r = (color.r + 1.0) / 2.0;
+    gl_FragColor = vec4(r, 0.0 , 0.0, 1.0);
 }
--- Aula-02/vertex.glsl
+++ Aula-02/vertex.glsl
@@ -2,7 +2,10 @@ precision highp float;
 
 attribute vec2 position;
 
+varying vec2 color;
+
 void main() {
+    color = position;
     gl_PointSize = 5.0;
     gl_Position = vec4(position, 0.0, 1.0);
 }
